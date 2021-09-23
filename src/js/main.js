ae819823018d31082d2deb23cea74c11dba4359c
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-24 00:40:25 +0300

    fetch chuck norris api random joke

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log("INITIAL LOG...");
+let URL = 'https://api.chucknorris.io/jokes/random';
+
+const getRamdomJoke = async () => {
+  try {
+    const response = await axios.get(URL);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getRamdomJoke();
