d53d38e52b9c9752b0cbbb4fcea087b27f651fab
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-24 00:45:19 +0300

    render avatar, category & random joke on HTML

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,25 @@ const getRamdomJoke = async () => {
   try {
     const response = await axios.get(URL);
     console.log(response.data);
+    refreshJoke();
+    updateJoke(response.data.value);
   } catch (error) {
     console.error(error);
   }
 }
 
 getRamdomJoke();
+
+const updateJoke = joke => {
+  document.getElementById('joke').textContent = joke;
+}
+
+const refreshJoke = () => {
+  document.getElementById('joke').textContent = '';
+}
+
+// HELPER Functions
+const capitalizeTxt = txt => {
+  // If you want lowercase the rest txt.slice(1).toLowerCase();
+  return txt.charAt(0).toUpperCase() + txt.slice(1);
+}
