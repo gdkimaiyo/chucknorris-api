a197e7ff834a5519f338fb9a572bf18e8b50a4ed
gdkimaiyo <gdkimaiyo@gmail.com>
2021-09-26 22:26:34 +0300

    feat: filter chuck norris jokes by category

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-let URL = 'https://api.chucknorris.io/jokes/random';
+const CATEGORIES = 'https://api.chucknorris.io/jokes/categories';
+let URL = randomUrl = 'https://api.chucknorris.io/jokes/random';
 
 const getRamdomJoke = async () => {
   try {
@@ -6,14 +7,58 @@ const getRamdomJoke = async () => {
     const response = await axios.get(URL);
     console.log(response.data);
     document.getElementById('spinner').style.display = 'none';
-    refreshJoke();
     updateJoke(response.data.value);
   } catch (error) {
     console.error(error);
   }
 }
+const getCategories = async () => {
+  try {
+    const response = await axios.get(CATEGORIES);
+    updateCategories(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-getRamdomJoke();
+getCategories();
+
+const updateCategories = categories => {
+  let select = document.getElementById('filter');
+  if (select.hasChildNodes()) {
+    removeAllChildNodes(select);
+  }
+  // Default Select
+  let defaultOpt = document.createElement('option');
+  defaultOpt.value = 'random';
+  defaultOpt.selected = true;
+  defaultOpt.textContent = "Random";
+
+  select.appendChild(defaultOpt);
+  // Generate other select options
+  categories.forEach((element, i) => {
+    let opt = document.createElement('option');
+    opt.className = "first-letter-uppercase"
+    opt.value = element;
+    opt.textContent = capitalizeTxt(element);
+
+    select.appendChild(opt);
+  });
+  changeCategory();
+}
+
+// Change Select / category
+const changeCategory = () => {
+  const select = document.getElementById('filter');
+  if (select.hasChildNodes()) {
+    const selected = select.options[select.selectedIndex].value;
+    document.getElementById('category-name').textContent = capitalizeTxt(selected);
+    refreshJoke();
+    URL = (selected === 'random') ? randomUrl : `https://api.chucknorris.io/jokes/random?category=${selected}`;
+    getRamdomJoke();
+  }
+}
+
 
 const updateJoke = joke => {
   document.getElementById('joke').textContent = joke;
@@ -27,4 +72,9 @@ const refreshJoke = () => {
 const capitalizeTxt = txt => {
   // If you want lowercase the rest txt.slice(1).toLowerCase();
   return txt.charAt(0).toUpperCase() + txt.slice(1);
+}
+const removeAllChildNodes = parent => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
