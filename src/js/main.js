e0c4c2f065869f3c8acbda43f66012e0976feb99
gdkimaiyo <gdkimaiyo@gmail.com>
2026-04-29 10:01:38 +0300

    fix: resolve URL constructor error and undefined randomUrl

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,18 @@
-const CATEGORIES = 'https://api.chucknorris.io/jokes/categories';
-let URL = randomUrl = 'https://api.chucknorris.io/jokes/random';
+const CATEGORIES = "https://api.chucknorris.io/jokes/categories";
+let RANDOM_BASE_URL = "https://api.chucknorris.io/jokes/random";
+let RANDOM_URL = "https://api.chucknorris.io/jokes/random";
 
 const getRamdomJoke = async () => {
   try {
-    document.getElementById('spinner').style.display = 'block';
-    const response = await axios.get(URL);
+    document.getElementById("spinner").style.display = "block";
+    const response = await axios.get(RANDOM_URL);
     console.log(response.data);
-    document.getElementById('spinner').style.display = 'none';
+    document.getElementById("spinner").style.display = "none";
     updateJoke(response.data.value);
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const getCategories = async () => {
   try {
@@ -20,69 +21,73 @@ const getCategories = async () => {
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 getCategories();
 
-const updateCategories = categories => {
-  let select = document.getElementById('filter');
+const updateCategories = (categories) => {
+  let select = document.getElementById("filter");
   if (select.hasChildNodes()) {
     removeAllChildNodes(select);
   }
   // Default Select
-  let defaultOpt = document.createElement('option');
-  defaultOpt.value = 'random';
+  let defaultOpt = document.createElement("option");
+  defaultOpt.value = "random";
   defaultOpt.selected = true;
   defaultOpt.textContent = "Random";
 
   select.appendChild(defaultOpt);
   // Generate other select options
   categories.forEach((element, i) => {
-    let opt = document.createElement('option');
-    opt.className = "first-letter-uppercase"
+    let opt = document.createElement("option");
+    opt.className = "first-letter-uppercase";
     opt.value = element;
     opt.textContent = capitalizeTxt(element);
 
     select.appendChild(opt);
   });
   changeCategory();
-}
+};
 
 // Change Select / category
 const changeCategory = () => {
-  const select = document.getElementById('filter');
+  const select = document.getElementById("filter");
   if (select.hasChildNodes()) {
     const selected = select.options[select.selectedIndex].value;
-    document.getElementById('category-name').textContent = capitalizeTxt(selected);
+    document.getElementById("category-name").textContent =
+      capitalizeTxt(selected);
     refreshJoke();
-    URL = (selected === 'random') ? randomUrl : `https://api.chucknorris.io/jokes/random?category=${selected}`;
+    RANDOM_URL =
+      selected === "random"
+        ? RANDOM_BASE_URL
+        : `https://api.chucknorris.io/jokes/random?category=${selected}`;
     getRamdomJoke();
   }
-}
+};
 
 // next joke button
-const nextJoke = evnt => {
+const nextJoke = (evnt) => {
   evnt.preventDefault();
   refreshJoke();
   getRamdomJoke();
-}
+};
 
-const updateJoke = joke => {
-  document.getElementById('joke').textContent = joke;
-}
+const updateJoke = (joke) => {
+  document.getElementById("joke").textContent = joke;
+};
 
 const refreshJoke = () => {
-  document.getElementById('joke').textContent = '';
-}
+  document.getElementById("joke").textContent = "";
+};
 
 // HELPER Functions
-const capitalizeTxt = txt => {
+const capitalizeTxt = (txt) => {
   // If you want lowercase the rest txt.slice(1).toLowerCase();
   return txt.charAt(0).toUpperCase() + txt.slice(1);
-}
+};
 
-const removeAllChildNodes = parent => {
+const removeAllChildNodes = (parent) => {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
-}
+};
